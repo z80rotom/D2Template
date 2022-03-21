4168442dff4fdaced46338e260226f1f152c01f8
z80rotom <z80rotom@protonmail.com>
2022-03-21 03:00:42 -0400

    Added patch to D2Game.dll
    Now patches D2Game.dll to allow you to open the Secret Cow Portal no
    matter the current quest state.

--- src/D2Patch.h
+++ src/D2Patch.h
@@ -38,7 +38,21 @@ static const DLLPatchStrc gptTemplatePatches[] =
         All your patches should be added here
         Keep it organized to save yourself some headache
     */
-    
+
+	// Quest reference:
+	// https://d2mods.info/forum/viewtopic.php?t=10620
+	// D2Game.dll
+	// 6fc2ebd6 0f  b6  4d  6d    MOVZX      ECX ,byte ptr [EBP  + 0x6d ] // get ptGame+6D (diff)
+	// 6fc2ebda 8b  44  88  10    MOV        EAX ,dword ptr [EAX  + ECX *0x4  + 0x10 ] // pHistory (diff)
+	// 6fc2ebde 6a  0a            PUSH       0xa // intermediate event / Killed Cow King
+	// 6fc2ebe0 6a  04            PUSH       0x4 // Cain quest
+	// 6fc2ebe2 50                PUSH       EAX
+	// 6fc2ebe3 e8  32  b9        CALL       D2COMMON.DLL::Ordinal_10156 // Call getQuestState
+	// 6fc2ebe8 85  c0            TEST       EAX ,EAX // Check quest state
+	// arg3 quest state
+	// arg2 quest ID
+	// arg1 ptHistory
+	{D2DLL_D2GAME, 0xEBE8, 0x33, FALSE, 0x1},
     {D2DLL_INVALID} // this must be the last entry in the array!
 };
 
